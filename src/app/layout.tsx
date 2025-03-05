2b6a2f3e67b571e087c855add0f8a4fabba85f8f
NISCHIT D S <105924980+Nischit17@users.noreply.github.com>
2025-03-05 11:09:56 +0530

    Initial commit on design-preloader branch (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import PreLoaderWrapper from "@/components/PreLoaderWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <PreLoaderWrapper>{children}</PreLoaderWrapper>
       </body>
     </html>
   );
